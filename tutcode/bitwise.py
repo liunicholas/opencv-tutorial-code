6ee361ec2c70f6a008a12025daaad07c74e66fc9
Bickolas Liu <51180693+liunicholas@users.noreply.github.com>
2020-08-31 09:04:15 -0700

    changed the folders
    changed the folders so that I only need one repo for adhon cs class

--- tutcode/bitwise.py
+++ tutcode/bitwise.py
@@ -11,18 +11,22 @@ circle = np.zeros((300, 300), dtype = "uint8")
 cv2.circle(circle, (150, 150), 150, 255, -1)
 cv2.imshow("Circle", circle)
 
+#both pixels greater than 0
 bitwiseAnd = cv2.bitwise_and(rectangle, circle)
 cv2.imshow("AND", bitwiseAnd)
 cv2.waitKey(0)
 
+#either pixel is greater than 0
 bitwiseOr = cv2.bitwise_or(rectangle, circle)
 cv2.imshow("OR", bitwiseOr)
 cv2.waitKey(0)
 
+#either but not both are greater than 0
 bitwiseXor = cv2.bitwise_xor(rectangle, circle)
 cv2.imshow("XOR", bitwiseXor)
 cv2.waitKey(0)
 
+#inverts on and off pixels
 bitwiseNot = cv2.bitwise_not(circle)
 cv2.imshow("NOT", bitwiseNot)
 cv2.waitKey(0)
